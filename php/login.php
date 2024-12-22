28f8c7e33b16b723e1667a36fb32256ffe1f5545
Hichamelhmaydi <ct1594065@gmail.com>
2024-12-22 16:31:46 +0100

    added Authentication

--- php/login.php
+++ php/login.php
@@ -1,12 +1,38 @@
 <?php
-$email_lo=$_POST["email_lo"];
-$password_lo=$_POST["password_lo"];
+include("connec.php");
+session_start();
+
+if (isset($_POST["sub"])) {
+    $email_lo = $_POST["email_lo"];
+    $password_lo = $_POST["password_lo"];
 
 
-$stmt = $conn->prepare("SELECT UserID, password_ FROM utilisateur WHERE email_lo = ?");
-$stmt->bind_param("s", $email_lo);
-$stmt->execute();
-$stmt->store_result();
-$stmt->close();
+   
+    $password_lo_md5 = md5($password_lo);
 
+
+    $stmt = $conn->prepare("SELECT * FROM utilisateur WHERE email = ?");
+    $stmt->bind_param("s", $email_lo);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+
+         if ($user['password_'] === $password_lo_md5) {
+            $_SESSION['user_email'] = $user['email'];
+            header("Location: ../index.php");
+            exit();   
+        } else {
+            echo "mots de passe incorrect";   
+        }
+    } else {
+        echo "email pas existé";   
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
 ?>
